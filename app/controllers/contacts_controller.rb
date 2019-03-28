b14a96e67268a153e22f94e1c79678e303d1eecb
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-03-28 12:25:43 +1100

    footer sized for mobile and desktop

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,10 +4,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.creation_confirmation(@contact).deliver_now
-      flash[:notice] = "Your enquiry has been sent. We'll be in touch #{@contact.name}!"
+      flash.now[:notice] = "Your enquiry has been sent. We'll be in touch #{@contact.name}!"
       redirect_to root_path
     else
-      flash[:alert] = "#{@contact.errors.count} errors. Please re-enter enquiry below."
+      flash.now[:alert] = "#{@contact.errors.count} errors. Please re-enter enquiry below."
       render "pages/home"
     end
   end
