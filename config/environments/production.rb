72bb2925fcc30d275be00edb43710b1e4a55b830
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-04-01 11:10:41 +1100

    changed sstp to 465

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,16 @@ Rails.application.configure do
 
   # email with gmail
   # config/environments/production.rb
-  Rails.application.configure do
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { host: "ritchiesgardens.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "ritchiesgardens.herokuapp.com" }
     # or your custom domain name eg. "www.yourdomain.com"
-  end
-
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   domain: 'gmail.com',
   user_name: ENV['GMAIL_ADDRESS'],
   password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
+  authentication: :plain,
   enable_starttls_auto: true
 }
 
