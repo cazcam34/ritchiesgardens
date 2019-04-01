92c7f46a5194bd58e57b764f670aea5bc9c9b4ab
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-04-01 11:00:12 +1100

    added mail to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,16 @@ Rails.application.configure do
     # or your custom domain name eg. "www.yourdomain.com"
   end
 
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
