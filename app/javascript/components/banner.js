0c2343e36a094eef4fbec9d8c7c71928375c45fc
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-04-05 16:37:45 +1100

    fixed the typed console error

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('span', {
-    strings: ["Welcome to Ritchie's Gardens", "Your trusted gardener"],
-    typeSpeed: 70,
-    loop: true
-  });
+  const type = document.querySelector('.typed');
+  if (type) {
+    new Typed('span', {
+      strings: ["Welcome to Ritchie's Gardens", "Your trusted gardener"],
+      typeSpeed: 70,
+      loop: true
+    });
+  };
 };
 
 export { loadDynamicBannerText };
