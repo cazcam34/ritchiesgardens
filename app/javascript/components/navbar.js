7f1bc72d76aef86abb42600df54d729acad0bee7
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-04-05 15:18:14 +1100

    fixed banner scroll to work on different banner heights

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,10 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-wagon');
+  const banner = document.querySelector('.banner');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      console.log(window.innerHeight)
+      if (window.scrollY >= banner.offsetHeight) {
         navbar.classList.add('navbar-wagon-black');
       } else {
         navbar.classList.remove('navbar-wagon-black');
